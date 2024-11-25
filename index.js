36a37101f8931df66327161f7b39aa409102f49d
Zach Latta <zach@zachlatta.com>
2024-11-24 19:19:24 -0500

    Try running sync in batches of 2500

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function processRecords() {
         console.log('Successfully connected to the database.');
 
         let offset = 0;
-        const limit = 10000;
+        const limit = 2500;
         let hasMore = true;
 
         let emailHeartbeatMap = {}; // To track emails and their latest heartbeat
